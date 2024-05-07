f9b5a939b658468336fca77887d801754cc64f07
Kyle Unverferth <kyle@kyleu.com>
2024-05-07 16:10:38 -0600

    JSON utility improvements, websocket refactor

--- app/services.go
+++ app/services.go
@@ -2,35 +2,20 @@ package app
 
 import (
 	"context"
-	"encoding/json"
 
 	"github.com/kyleu/solitaire/app/lib/websocket"
 	"github.com/kyleu/solitaire/app/util"
 )
 
 type Services struct {
+	CoreServices
 	Socket *websocket.Service
 }
 
 func NewServices(ctx context.Context, st *State, logger util.Logger) (*Services, error) {
-	return &Services{
-		Socket: websocket.NewService(nil, socketHandler, nil),
-	}, nil
+	return &Services{CoreServices: initCoreServices(ctx, st, logger)}, nil
 }
 
 func (s *Services) Close(_ context.Context, _ util.Logger) error {
-	return nil
-}
-
-func socketHandler(_ context.Context, s *websocket.Service, c *websocket.Connection, _ string, cmd string, _ json.RawMessage, logger util.Logger) error {
-	switch cmd {
-	case "connect":
-		_, err := s.Join(c.ID, "tap", logger)
-		if err != nil {
-			return err
-		}
-	default:
-		logger.Error("unhandled command [" + cmd + "]")
-	}
 	return nil
 }
