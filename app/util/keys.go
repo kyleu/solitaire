0f5ec53a4f481d94a64df89003bd43a748e7348f
Kyle Unverferth <kyle@kyleu.com>
2024-06-15 12:47:59 -0600

    Linting fixes

--- app/util/keys.go
+++ app/util/keys.go
@@ -11,6 +11,7 @@ const (
 	AppSource  = "https://github.com/kyleu/solitaire"
 	AppLegal   = `Built by <a href="mailto:solitaire@kyleu.com">Kyle U</a>, all rights reserved`
 
+	KeyDebug   = "debug"
 	KeyError   = "error"
 	KeyRandom  = "random"
 	KeyUnknown = "unknown"
