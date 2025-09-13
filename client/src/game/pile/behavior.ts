81636c5b67be6aa6de438bbaeb1cb158b9bdae0f
Kyle Unverferth <kyle@kyleu.com>
2025-09-13 14:25:27 -0600

    Utility methods

--- client/src/game/pile/behavior.ts
+++ client/src/game/pile/behavior.ts
@@ -8,3 +8,18 @@ export enum Behavior {
   Reserve = "reserve",
   Pyramid = "pyramid"
 }
+
+export function parseBehavior(value: string): Behavior | undefined {
+  if (Object.values(Behavior).includes(value as Behavior)) {
+    return value as Behavior;
+  }
+  return undefined;
+}
+
+export function getBehavior(value: string): Behavior {
+  const x = parseBehavior(value);
+  if (x === undefined) {
+    throw new Error(`invalid [Behavior]: ${value}`);
+  }
+  return x;
+}
--- client/src/game/suit/color.ts
+++ client/src/game/suit/color.ts
@@ -6,3 +6,18 @@ export enum Color {
   Colorless = "colorless",
   Unknown = "unknown"
 }
+
+export function parseColor(value: string): Color | undefined {
+  if (Object.values(Color).includes(value as Color)) {
+    return value as Color;
+  }
+  return undefined;
+}
+
+export function getColor(value: string): Color {
+  const x = parseColor(value);
+  if (x === undefined) {
+    throw new Error(`invalid [Color]: ${value}`);
+  }
+  return x;
+}
